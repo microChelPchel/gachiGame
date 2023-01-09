54f1f4a52d1b60035b5f4da1679525c82c376550
Sokolov Alexandr <sachacok@yandex.ru>
2023-01-09 19:19:55 +0300

    update game cycle

--- src/main/java/base/Game.java
+++ src/main/java/base/Game.java
@@ -6,6 +6,9 @@ import java.awt.*;
 
 public class Game implements Runnable {
 
+    private final int FPS_SET = 120;
+    private final int UPS_SET =200;
+
     private GamePanel gamePanel;
     private GameWindow gameWindow;
     private Player player;
@@ -40,10 +43,45 @@ public class Game implements Runnable {
     @Override
     public void run() {
 
-        while (true) {
-            update();
-        }
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
 
+        long previousTime = System.nanoTime();
+
+        int frames = 0;
+        int updates = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        double deltaU = 0;
+        double deltaF = 0;
+
+        while (true) {
+            long currentTime = System.nanoTime();
+
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
+
+            if (deltaU >= 1) {
+                update();
+                updates++;
+                deltaU--;
+            }
+
+            if (deltaF >= 1) {
+                gamePanel.repaint();
+                frames++;
+                deltaF--;
+            }
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
+                frames = 0;
+                updates = 0;
+
+            }
+        }
     }
 
 
